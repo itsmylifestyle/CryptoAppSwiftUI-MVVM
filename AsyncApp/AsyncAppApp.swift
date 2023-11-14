d663e657bd1e7afe9137a96c7ec024a6b91fe692
itsmylifestyle <hahaii00@mail.ru>
2023-11-15 01:14:42 +0600

    Initial Commit

---
+++ AsyncApp/AsyncAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAppApp.swift
+//  AsyncApp
+//
+//  Created by Айбек on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
